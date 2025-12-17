69ca96db6d37128eaab2fd55229259d6986fed53
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-17 15:22:17 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -80,7 +80,7 @@ let package = Package(
         .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.4.0"),
         .package(url: "https://github.com/swift-standards/swift-standards", from: "0.1.0"),
         .package(url: "https://github.com/swift-standards/swift-iso-9899", from: "0.1.0"),
-        .package(url: "https://github.com/swift-standards/swift-html-standard", from: "0.1.0"),
+        .package(url: "https://github.com/swift-standards/swift-html-standard", from: "0.1.4"),
         .package(url: "https://github.com/swift-standards/swift-w3c-css", from: "0.1.0"),
     ],
     targets: [
