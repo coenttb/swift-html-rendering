353fe0a9f8c6951714562c6a0d2a33c0294e85cf
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-17 15:22:36 +0100

    use swift-whatwg-html HTML namespace

--- Sources/HTML Renderable/HTML.swift
+++ Sources/HTML Renderable/HTML.swift
@@ -5,23 +5,7 @@
 //  Created by Coen ten Thije Boonkkamp on 26/11/2025.
 //
 
-/// Namespace for HTML-related types.
-///
-/// The `HTML` enum provides a namespace for all HTML rendering types,
-/// similar to how SwiftUI uses namespacing. The main protocol is
-/// available as `HTML.View`.
-///
-/// Example:
-/// ```swift
-/// struct MyPage: HTML.View {
-///     var body: some HTML.View {
-///         HTML.Element(tag: "div") {
-///             HTML.Text("Hello, World!")
-///         }
-///     }
-/// }
-/// ```
-public enum HTML {}
+import W3C_CSS_Shared
 
 extension HTML {
     /// &quot; - Double quotation mark HTML entity
